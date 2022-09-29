ee2051a63853de2754f21ac4078f9e2351060667
YingzheJin <63117938+YingzheJin@users.noreply.github.com>
2022-09-29 19:14:41 -0400

    add cutoff setting

--- port_evaluation/classifier/run_classifier.py
+++ port_evaluation/classifier/run_classifier.py
@@ -1,5 +1,14 @@
 from load_data import import_data
 from suspicious import classify
 
-data = import_data()
-classified_data = classify(data)
+
+# the hyperparameter to be tuned
+    # if cutoff*encounter >= (1-cutoff)*loitering then we set label = 0 as unsuspicious
+    # if cutoff*encounter < (1-cutoff)*loitering then we set label = 1 as suspicious
+cutoff = 0.66
+
+
+carriers, encounter, loitering = import_data()
+
+#classified_data return the carriers info with new cols: num of encounter, num of loitering and label
+classified_data = classify(carriers, encounter, loitering, cutoff)
