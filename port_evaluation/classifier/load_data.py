30ee77821ef2e57b9ac97c81af43d7bab1531128
YingzheJin <63117938+YingzheJin@users.noreply.github.com>
2022-09-29 19:14:09 -0400

    Dylan change load data sheet

--- port_evaluation/classifier/load_data.py
+++ port_evaluation/classifier/load_data.py
@@ -4,7 +4,6 @@ def import_data():
     path = 'port_evaluation/data/xlsx/gfw_data.xlsx'
     xls = pd.ExcelFile(path)
     dfList = []
-    sheetsList = xls.sheet_names
-    for sheet in sheetsList:
+    for sheet in ['carriers', 'encounter', 'loitering']:
         dfList.append(pd.read_excel(path, sheet_name=sheet))
     return dfList
