c1ed66d80bc3e3f8ab0fda6007901403bf0a5aa3
sebdodt <mats.dodt@googlemail.com>
2022-10-04 21:05:38 -0400

    changed folder structure

--- port_evaluation/src/converter.r
+++ port_evaluation/src/converter.r
@@ -2,7 +2,7 @@ library(openxlsx)
 
 # reading in data
 source("port_evaluation/src/loader.r")
-data <- load_RDS_data("proc")
+data <- load_RDS_data("input/GFW/api/unlisted")
 
 # writing data
-openxlsx::write.xlsx(data, "port_evaluation/data/xlsx/gfw_data.xlsx")
+openxlsx::write.xlsx(data, "port_evaluation/data/proc/GFW/gfw_data-v3.xlsx")
