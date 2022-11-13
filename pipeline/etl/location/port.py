59bfe8acf7d94f00e172853e61c1622a9f52fe96
IreneZiyouLi <irene.ziyou.li@gmail.com>
2022-11-13 16:16:10 -0500

    add binary variable "at_port" from Seavision

--- pipeline/etl/location/port.py
+++ pipeline/etl/location/port.py
@@ -1,6 +1,7 @@
+import pandas as pd
+import numpy as np
 
-
-def at_port():
+def at_port(output):
     '''
     appends columns:
     - 'at_port': whether ship is currently at port
@@ -8,4 +9,13 @@ def at_port():
     - 'name_port': name of the port that the ship is at
     - 'country_port': country of the port where the ship is at
     '''
-    pass
+    ### assume ouput is as type of dataframe
+    output = pd.DataFrame(output)
+    seaVision_file = r"C:\Users\Irene\Desktop\Fall22\Capstone Project\Data\lists-Reefers-2022-11-11_04-40.csv"
+    sea_vision= pd.read_csv(seaVision_file)
+    ### can be reused for other functions
+    ############
+    at_port_data = sea_vision[["MMSI", "Navigation Status"]]
+    at_port_data['at_port'] = np.where(at_port_data["Navigation Status"]=="5-Moored", 1, 0)
+    output_1 = output.merge(at_port_data, left_on="mmsi", right_on="MMSI",how = "left")
+    return output_1
