78e8a521579263e53b5d60c0d323f851c7077b40
IreneZiyouLi <irene.ziyou.li@gmail.com>
2022-11-21 23:32:40 -0500

    Update eez.py

--- pipeline/etl/location/eez.py
+++ pipeline/etl/location/eez.py
@@ -1,4 +1,8 @@
-
+import geopandas as gpd
+from shapely.geometry import mapping
+import pandas as pd
+from shapely.geometry import Point
+import numpy as np
 
 def in_eez(output):
     '''
@@ -9,4 +13,25 @@ def in_eez(output):
     - 'in_eez': whether boat is currently in any EEZ
     - 'eez': name of the EEZ where boat is currently in
     '''
-    return output
+
+    ### to do: change the file name, this is the file name&oath of SEAVISION data file
+    file_name = r"C:\Users\Irene\Desktop\Fall22\Capstone Project\Data\lists-Reefers-2022-11-11_04-40.csv"
+    df= pd.read_csv(file_name)
+    df['coordinates'] = list(zip(df['Latitude'], df['Longitude']))
+    df.coordinates = df.coordinates.apply(Point)
+    ### to do: change the file name, this is the file name&path of EEZ Shapfile 
+    Shapefile = r"C:\Users\Irene\Desktop\Fall22\Capstone Project\Data\World_ECS_v1_20221013\World_ECS_v1_20221013\ecs_v01.shp"
+    gdf_polygons = gpd.read_file(Shapefile)
+    gdf_points = gpd.GeoDataFrame(df, geometry='coordinates')
+    gdf_points.crs = gdf_polygons.crs
+    sjoin = gpd.sjoin(gdf_points, gdf_polygons, how='left', op='within')
+    df_join = pd.DataFrame(sjoin)
+    result = df_join[["MMSI", "Latitude", "Longitude", "MRGID_TER1", "TERRITORY1", "TERRITORY2","SOVEREIGN1", "SOVEREIGN2"]]
+    ## Append useful columns!
+    result["in_eez"] = np.where(result['MRGID_TER1'].isna(), 0, 1)
+    us_iso = "USA"
+    # five eyes countries: Australia, Canada, New Zealand, the United Kingdom and the United States
+    five_eyes_iso =["USA", "AUS", "CAN", "NZL","GBR"] 
+    result['in_us_eez'] = np.where(result['ISO_SOV1']== "USA", 1, 0)
+    result['in_five_eyes_eez']= result.apply(lambda x: 1 if x['ISO_SOV1'] in five_eyes_iso else 0, axis=1 )
+    return result
