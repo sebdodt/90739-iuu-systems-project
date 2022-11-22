0e31ade971cb80405fa469c347308d2871430150
IreneZiyouLi <irene.ziyou.li@gmail.com>
2022-11-21 22:35:51 -0500

    Update ship.py
    append info columns to the final output table

---
+++ pipeline/etl/info/ship.py
@@ -0,0 +1,23 @@
+import pandas as pd
+def mmsi_info(output):
+    '''
+    appends columns  for ship's information
+    - 'ETA': estimate time of arrival
+    - 'Call Sign': unique alphanumeric identity that belongs to the vessel
+    - 'Name': name of the carrier
+    - 'Year built'
+    - 'latitude'
+    - 'longtitude'
+    - 'Heading': the direction the vessels is going 
+    - 'flag'
+    - 'deadweight': weight
+    '''
+    ### assume ouput is as type of dataframe
+    output = pd.DataFrame(output)
+    ### **** to do: change the file name
+    seaVision_file = r"C:\Users\Irene\Desktop\Fall22\Capstone Project\Data\lists-Reefers-2022-11-11_04-40.csv"
+    sea_vision= pd.read_csv(seaVision_file)
+    ### can be reused for other functions
+    info_data = sea_vision[["ETA", 'Call Sign', "Name", 'Year Built', 'Latitude','Longitude', 'Heading', 'Flag', 'Deadweight', "MMSI"]]
+    new_output = output.merge(info_data, left_on="MMSI", right_on="MMSI",how = "left")
+    return new_output
