07e4d9c0496ccb6e75d562ed005f929b9f36088a
IreneZiyouLi <irene.ziyou.li@gmail.com>
2022-11-22 00:37:45 -0500

    Create test_etl.py

---
+++ pipeline/etl/test_etl.py
@@ -0,0 +1,14 @@
+from info.ship import *
+from location.eez import *
+from location.port import *
+import geopandas as gpd
+from shapely.geometry import mapping
+import pandas as pd
+from shapely.geometry import Point
+import numpy as np
+### the output file from Dylan side
+output_filename = r"C:\Users\Irene\Desktop\Fall22\Capstone Project\Data\carrier_mmsi.csv"
+output = pd.read_csv(output_filename)
+result = mmsi_info(output)
+result = at_port(pd.DataFrame(result))
+result = in_eez(pd.DataFrame(result))
