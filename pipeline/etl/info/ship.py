5d03594edcc092371ae51b7ede906c82b298aa8a
IreneZiyouLi <irene.ziyou.li@gmail.com>
2022-11-26 18:35:57 -0500

    Update ship.py

--- pipeline/etl/info/ship.py
+++ pipeline/etl/info/ship.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+output_filename = r"C:\Users\Irene\Desktop\Fall22\Capstone Project\Data\carrier_mmsi.csv"
+output = pd.read_csv(output_filename)
 def mmsi_info(output):
     '''
     appends columns  for ship's information
@@ -12,6 +14,10 @@ def mmsi_info(output):
     - 'Heading': the direction the vessels is going 
     - 'flag'
     - 'deadweight': weight
+    - 'Registered Owner'
+    - 'Speed'
+    - 'Builder' 
+    - 'Port Of Registry'
     '''
     ### assume ouput is as type of dataframe
     output = pd.DataFrame(output)
@@ -19,7 +25,7 @@ def mmsi_info(output):
     seaVision_file = r"C:\Users\Irene\Desktop\Fall22\Capstone Project\Data\lists-Reefers-2022-11-11_04-40.csv"
     sea_vision= pd.read_csv(seaVision_file)
     ### can be reused for other functions
-    info_data = sea_vision[["ETA", 'Call Sign', "Name", 'Year Built', 'Latitude','Longitude', 'Heading', 'Flag', 'Deadweight', "MMSI"]]
+    info_data = sea_vision[["ETA", 'Call Sign', "Name", 'Year Built', 'Latitude','Longitude', 'Heading', 'Flag', 'Deadweight', "MMSI", 'Registered Owner', 'Speed', 'Builder', 'Port Of Registry']]
     new_output = output.merge(info_data, left_on="MMSI", right_on="MMSI",how = "left")
     return new_output
     
