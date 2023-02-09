f23fb4964dbb8acaba1f7374b24b0d8e9dbd8177
sebdodt <mats.dodt@googlemail.com>
2023-02-09 09:01:27 -0500

    added documentation to network.py

--- port_evaluation/eda/midterm/1_reefers/network.py
+++ port_evaluation/eda/midterm/1_reefers/network.py
@@ -60,27 +60,39 @@ def data_port(mmsis, port_visits):
 
 
 def make_plot(name, start, end, top_n, edge_threshold, encounters, mmsi, loitering, port_visits):
+
+    # filter respective time period
     loitering = loitering.loc[loitering['start'] >= start,:]
     loitering = loitering.loc[loitering['start'] <= end,:]
     port_visits = port_visits.loc[port_visits['event_start'] >= start,:]
     port_visits = port_visits.loc[port_visits['event_start'] <= end,:]
+
+    # group and count by vessel
     loitered_grouped = loitering.groupby(['vessel.mmsi'])['id'] \
         .count().reset_index() \
-        .rename({'id':'loitering', 'vessel.mmsi':'mmsi'}, axis=1) # , 'vessel.name':'Name'
-    loitered_grouped.sort_values('loitering', ascending=False, inplace=True)
+        .rename({'id':'loitering', 'vessel.mmsi':'mmsi'}, axis=1) \
+        .sort_values('loitering', ascending=False, inplace=True)
+    
+    # filter top n number of vessels
     top_100_loiterers = loitered_grouped['mmsi'].head(top_n)
-    # top_100_names = loitered_grouped['Name'].head(top_n).reset_index(drop=True)
-    n_loitering = np.log(loitered_grouped['loitering'].head(top_n).reset_index(drop=True)) # maybe add log
-    # top_100_flags = mmsi['Flag'].head(top_n).reset_index(drop=True)
-    # top_100_owner = mmsi['Registered Owner'].head(top_n).reset_index(drop=True)
+
+    # take log of loitering to make points in graph more similar of size
+    n_loitering = np.log(loitered_grouped['loitering'].head(top_n).reset_index(drop=True))
+
+    # filtering encounters of top n loitering vessels
     encounters = encounters.loc[encounters['vessel.mmsi'].isin(top_100_loiterers),:]
+
+    # filtering time period of encounters
     encounters = encounters.loc[encounters['start'] >= start,:]
     encounters = encounters.loc[encounters['start'] <= end,:]
+
+    # getting ship name for each ship
     ship_indices = list(encounters['vessel.mmsi'].unique())
     ship_names = {}
     for ship in ship_indices:
         ship_names[ship] = encounters.loc[encounters['vessel.mmsi']==ship,'vessel.name'].head(1).values[0]
     
+    # creating an n x n matrix with all connections between reefers
     E = np.zeros((len(ship_indices),len(ship_indices)))
     for x in range(len(ship_indices)):
         for y in range(len(ship_indices)):
@@ -90,13 +102,18 @@ def make_plot(name, start, end, top_n, edge_threshold, encounters, mmsi, loiteri
             encounters_of_x = len(encounters_of_fishers.loc[encounters_of_fishers['vessel.mmsi']==ship_indices[x],:])
             E[x,y] = encounters_of_x
 
+    # initialise graph object
     G = nx.Graph()
     ship_idx = range(len(ship_indices))
+
+    # add nodes
     for i in ship_idx:
         G.add_node(
             i)#, 
             #label=top_100_names.reset_index(drop=True)[i])
             #size=n_loitering[i]/max(n_loitering) * 5)
+
+    # add edges
     for i in ship_idx:
         for j in ship_idx:
             if i > j:
@@ -107,12 +124,11 @@ def make_plot(name, start, end, top_n, edge_threshold, encounters, mmsi, loiteri
     net.from_nx(G)
     # net.show('port_evaluation/output/networks/new_{name}_weighted_logged.html'.format(name=name))
 
-    ## new attempt multiple plots
 
-    degree_sequence = sorted((d for n, d in G.degree()), reverse=True)
-    dmax = max(degree_sequence)
+    # sorting clusters by size
     networks = sorted(nx.connected_components(G), key=len, reverse=True)
 
+    # owners of chinese ships for a different analysis. Can be ignored
     chinese = [
         'China National Fisheries Corporation',
         'Wei Fong Shipping Co Ltd',
@@ -126,36 +142,46 @@ def make_plot(name, start, end, top_n, edge_threshold, encounters, mmsi, loiteri
         'Zhoushan Haili Ocean Fisheries'
         ]
 
-
+    # returning a plot for each network
     for i_net in range(len(networks)):
+
+        # setting a threshold for minimum number of nodes in graph
         if len(networks[i_net]) >= 4:
+
+            # Title an size
             fig = plt.figure("Degree of a random graph", figsize=(10.7, 6))
             axgrid = fig.add_gridspec(18, 32)
-
             ax0 = fig.add_subplot(axgrid[:, -18:])
             Gcc = G.subgraph(networks[i_net])
             pos = nx.spring_layout(Gcc, seed=10396953)
+
 
             pos_labels = {}
             label_names = {}
             node_weight = []
             mmsis = []
             for node, coords in pos.items():
+
+                # adding vessel name as label
                 pos_labels[node] = (coords[0], coords[1] + 0.05)
                 label_names[node] = ship_names[ship_indices[node]].title()
+
+                # storing mmsi numbers of vessels in network for later
                 mmsis.append(ship_indices[node])
                 node_weight.append(loitered_grouped.loc[loitered_grouped['mmsi']==ship_indices[node],'loitering'].values[0] /max(n_loitering) * 1.8)
+
+            # changing size of edge depending on strength of connection
             edge_weights = list(nx.get_edge_attributes(G,'weight').values())
 
-            nx.draw_networkx_nodes(Gcc, pos, ax=ax0,  label=True, node_size=node_weight) # node_size=20, 
+            # plotting the network graph
+            nx.draw_networkx_nodes(Gcc, pos, ax=ax0,  label=True, node_size=node_weight)
             nx.draw_networkx_edges(Gcc, pos, ax=ax0, alpha=0.25, width=edge_weights)
             nx.draw_networkx_labels(Gcc,pos_labels, ax=ax0, labels=label_names, font_size=7)
             ax0.set_title("Network {i} in {year}".format(i=i_net+1, year=start[:4]))
             ax0.set_axis_off()
 
-            ports = data_port(mmsis, port_visits)
+            # plotting the rfmos of the network
             rfmos = data_rfmo(mmsis, loitering)
-
             ax1 = fig.add_subplot(axgrid[:6, :12])
             sns.barplot(data=rfmos.head(10), 
                 x='rfmo', 
@@ -168,6 +194,8 @@ def make_plot(name, start, end, top_n, edge_threshold, encounters, mmsi, loiteri
             ax1.set_ylabel("% of dark encounters")
             ax1.set_xlabel("")
 
+            # plotting the ports of the network
+            ports = data_port(mmsis, port_visits)
             ax2 = fig.add_subplot(axgrid[10:16, :12])
             sns.barplot(data = ports.head(10), x='name1', y='event_id', ax=ax2, color=color)
             ax2.tick_params(labelrotation=90)
@@ -176,7 +204,7 @@ def make_plot(name, start, end, top_n, edge_threshold, encounters, mmsi, loiteri
             ax2.set_xlabel('')
             fig.tight_layout()
             plt.rcParams['savefig.transparent'] = True
-            plt.savefig('port_evaluation/output/networks/10 dec/new_{name}_export2_{i}_p_trans.png'.format(name=name, i=i_net+1), dpi=800)
+            plt.savefig('port_evaluation/output/networks/9 feb/{name}_{i}.png'.format(name=name, i=i_net+1), dpi=800)
             plt.clf()
 
 
@@ -193,15 +221,17 @@ if __name__=='__main__':
     port_visits.loc[~(port_visits['vessel_ssvid'].isin(not_reefer)),:]
 
 
-    for year in [2022]: #range(2012,2023):
-        for edge_threshold in [20]:# [11,12,13,14,15,16,17,18,19]:# [5, 7, 10, 20]:
+    # parameters 
+    years = [2022] #range(2012,2023):
+    edge_thresholds = [20]
+    top_n_filtered = [130]
+
+    for year in years: 
+        for edge_threshold in edge_thresholds:
             print(" > Generating network for {year} and thre. {t}...".format(year=year, t=edge_threshold))
             start = '{year}-01-01'.format(year=year)
             end = '{year}-12-31'.format(year=year)
-            for top_n in [130]:
-                # top_n = 140
-                # edge_threshold = 7
-                
+            for top_n in top_n_filtered:
                 name = '{year}_{t}_{n}'.format(year=year, t=edge_threshold, n=top_n)
                 make_plot(name, start, end, top_n, edge_threshold, encounters, mmsi, loitering, port_visits)
     print(" > Done.")
